5bd0a79913b2adbb78036d0c08c6dda80d821a8d
mgomri <mel77gomri@gmail.com>
2020-05-18 13:08:01 -0400

    initial push

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`sever is listening on port ${PORT}...`)
+})
